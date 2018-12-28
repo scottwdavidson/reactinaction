135e1f98487fdfb6ca50014405a64d7e289ded1e
Scott Davidson <scottwdavidson@users.noreply.github.com>
2018-12-28 11:07:33 -0800

    Added child comment objects

--- chap2/section2.2.3/src/index.js
+++ chap2/section2.2.3/src/index.js
@@ -1,19 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
+import PropTypes from "prop-types";
 const root = document.getElementById("root");
-const virtualRoot = React.createElement(
-  "div",
-  {},
-  React.createElement(
-    "h1",
-    {},
-    "Hello, world!",
-    React.createElement(
-      "a",
-      { href: "mailto:scottd@tiogasecurity.com" },
-      React.createElement("h1", {}, "React in Action"),
-      React.createElement("em", {}, "... and now it really is")
-    )
-  )
+class Post extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "post"
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "postAuthor",
+          id: this.props.id
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "postBody"
+          },
+          this.props.content
+        ),
+        this.props.children
+      )
+    );
+  }
+}
+
+Post.propTypes = {
+  user: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
+};
+
+class Comment extends Component {
+  render() {
+    console.log("yo - rendering here");
+    return React.createElement(
+      "div",
+      {
+        className: "comment"
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "commentAuthor"
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "commentContent"
+          },
+          this.props.content
+        )
+      )
+    );
+  }
+}
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired
+};
+const aComment = React.createElement(Comment, {
+  id: 3,
+  user: "ian",
+  content: " commented: f you, you nerd!"
+});
+const App = React.createElement(
+  Post,
+  {
+    id: 1,
+    content: " said: this is a post!",
+    user: "Ian"
+  },
+  React.createElement(Comment, {
+    id: 2,
+    user: "bob",
+    content: " commented: wow! how cool!"
+  }),
+  aComment
 );
-render(virtualRoot, root);
+
+render(App, root);
